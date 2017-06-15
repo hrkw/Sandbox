260d6b7edb4f2a2dfe3582f045410f414b27aaf3
kazuko-h <kazuko-h@Z1.kanda.natswell.com>
2017-06-15 18:35:07 +0900

    マージテスト

--- src/sample/Sample20170615.java
+++ src/sample/Sample20170615.java
@@ -3,5 +3,11 @@ package sample;
 public class Sample20170615 {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		test();
 	}
+
+	public static void test(){
+		System.out.println("test!");
+	}
+
 }
