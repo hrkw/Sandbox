77c858b742e2e7a108961f1ca95c77cc342a85e7
nao-e <nao-e@192.168.100.129>
2017-06-15 18:35:49 +0900

    nao-e テストコミット

--- src/sample/Sample20170615.java
+++ src/sample/Sample20170615.java
@@ -3,5 +3,13 @@ package sample;
 public class Sample20170615 {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		getProperty();
+	}
+
+	public static void getProperty(){
+	    System.out.print("Javaバージョン(java.version):");
+	    System.out.println(System.getProperty("java.version"));
+	    System.out.print("オペレーティングシステム名(os.name):");
+	    System.out.println(System.getProperty("os.name"));
 	}
 }
